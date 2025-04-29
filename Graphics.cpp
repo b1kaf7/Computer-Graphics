d58d5561a803c13101676636512ae00dd3dbe54d
b1kaf7 <kaflebinamra@gmail.com>
2025-04-29 19:39:09 +0545

    Bresenham’s Line Algorithm

--- Graphics.cpp
+++ Graphics.cpp
@@ -2,43 +2,43 @@
 #include <stdio.h>
 #include <math.h>
 #include <dos.h>
-int main( )
+int main()
 {
-float x,y,x1,y1,x2,y2,dx,dy,stepLength;
-int i;
-int gd=DETECT,gm;//graphic mode initialize
+    float x, y, x1, y1, x2, y2, dx, dy, stepLength;
+    int i;
+    int gd = DETECT, gm; // graphic mode initialize
 
-initgraph(&gd,&gm,NULL);
+    initgraph(&gd, &gm, NULL);
 
-printf("Enter the value of x1 and y1 : ");
-scanf("%f%f",&x1,&y1);
-printf("Enter the value of x2 and y2: ");
-scanf("%f%f",&x2,&y2);
+    printf("Enter the value of x1 and y1 : ");
+    scanf("%f%f", &x1, &y1);
+    printf("Enter the value of x2 and y2: ");
+    scanf("%f%f", &x2, &y2);
 
-dx=abs(x2-x1);
-dy=abs(y2-y1);
+    dx = abs(x2 - x1);
+    dy = abs(y2 - y1);
 
-if(dx>=dy)
-stepLength=dx;
-else
-stepLength=dy;
+    if (dx >= dy)
+        stepLength = dx;
+    else
+        stepLength = dy;
 
-dx=dx/stepLength;
-dy=dy/stepLength;
+    dx = dx / stepLength;
+    dy = dy / stepLength;
 
-x=x1;
-y=y1;
+    x = x1;
+    y = y1;
 
-i=1;
-while(i<=stepLength)
-{
-putpixel(x,y,WHITE);
-x=x+dx;
-y=y+dy;
-i=i+1;
-}
-delay(100000);
-//getch();
-closegraph();
-return 0;
+    i = 1;
+    while (i <= stepLength)
+    {
+        putpixel(x, y, WHITE);
+        x = x + dx;
+        y = y + dy;
+        i = i + 1;
+    }
+    delay(100000);
+    // getch();
+    closegraph();
+    return 0;
 }
